f1cdeaefb86a7bb5db44c471da2c7baa5ff696f9
Totenkopf <pysskuu88@icloud.com>
2023-03-12 22:49:51 +0300

    Добавлен Context. Добавлен компонент сортировки(Sort). Добавлена сортировка по цене скидке

--- landingpage14313/src/components/App/app.jsx
+++ landingpage14313/src/components/App/app.jsx
@@ -4,21 +4,34 @@ import Footer from "../Footer/footer";
 import Header from "../Header/header";
 import Tagline from "../Tagline/tagline";
 import data from '../../assets/data.json';
+import { CardContext } from '../../context/cardContext';
 
 function App() {
 
     const [cards,setCards]=useState(data);
+    const [currentSort, setCurrentSort] = useState("");
+
+    const sortedData = (currentSort) => {
+
+        switch (currentSort) {
+          case 'low': setCards(cards.sort((a, b) => b.price - a.price)); break;
+          case 'cheap': setCards(cards.sort((a, b) => a.price - b.price)); break;
+          case 'sale': setCards(cards.sort((a, b) => b.discount - a.discount)); break;
+          default: setCards(cards.sort((a, b) => a.price - b.price)); break;
+        }
+      }
 
     return (
+        <CardContext.Provider value={{ cards,  currentSort, sortedData: sortedData, setCurrentSort }}>
         <>
             <Header />
             <main className='content container'>
-                <CardList goods={cards}/>
+                <CardList/>
                 <Tagline />
             </main>
             <Footer />
         </>
-
+        </CardContext.Provider>
     )
 }
 
