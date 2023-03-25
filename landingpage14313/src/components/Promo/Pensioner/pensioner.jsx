e2b2aef33d017882aa90f8a3676a0fd825225209
Totenkopf <pysskuu88@icloud.com>
2023-03-25 22:59:01 +0300

    Поправил стили футера ++ добавил ссылу на телеграмм ++ поправил стили арусели

--- landingpage14313/src/components/Promo/Pensioner/pensioner.jsx
+++ landingpage14313/src/components/Promo/Pensioner/pensioner.jsx
@@ -4,8 +4,7 @@ import "./index.css"
 function Pensioner() {
     return (
         <div className='pensioner'>
-           
-            <img src={imgPensioner} alt="Скидка пенсионерам" className='pensioner__img' />
+           <img src={imgPensioner} alt="Скидка пенсионерам" className='pensioner__img' />
             <span className='pencia__15'>Скидка -15%</span>
             <span className='pencia__mir'>При оплате картой мир</span>
         </div>
